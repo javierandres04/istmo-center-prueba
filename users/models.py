805c1f7da1223d96d10abff448fb3a22ca8c30cd
Javier Molina <javmoli045@gmail.com>
2025-02-16 12:23:07 -0600

    added override to save in User Model to set is_staff based on role

--- users/models.py
+++ users/models.py
@@ -7,9 +7,15 @@ class User(AbstractUser):
         ADMIN = 'ADMIN', 'Admin'
     
     role = models.CharField(max_length=5, choices=Role.choices, default=Role.USER)
-    email = models.EmailField(unique=True)
+    username = models.EmailField(unique=True)
     name = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
+   
 
-
-    
+    def save(self, *args, **kwargs):
+        # If the user is an admin, set is_staff
+        if self.role == self.Role.ADMIN:
+            self.is_staff = True
+        else:
+            self.is_staff = False
+        super().save(*args, **kwargs)
