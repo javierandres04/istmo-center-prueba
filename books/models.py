bdc9c8dd258bc2835d3863ac2f48598e19a69012
Javier Molina <javmoli045@gmail.com>
2025-02-17 10:48:31 -0600

    added book loan model

--- books/models.py
+++ books/models.py
@@ -9,3 +9,14 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class BookLoan(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    loan_date = models.DateTimeField(auto_now_add=True)
+    return_date = models.DateTimeField(null=True)
+    returned = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.book.title} - {self.user.email}'
