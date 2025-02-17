6c1a85a690819d561d392354ceae6646db328241
Javier Molina <javmoli045@gmail.com>
2025-02-17 15:29:12 -0600

    added logging in views error handler

--- core/decorators/views_error_handling.py
+++ core/decorators/views_error_handling.py
@@ -1,6 +1,9 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.exceptions import ValidationError, NotFound
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def handle_view_exceptions(func):
@@ -8,9 +11,12 @@ def handle_view_exceptions(func):
         try:
             return func(self, request, *args, **kwargs)
         except ValidationError as e:
+            logger.error(f"Validation Error When Processing Request: {e.detail}")
             return Response({"error": e.detail}, status=status.HTTP_400_BAD_REQUEST)
         except NotFound as e:
+            logger.error(f"Resource Not Found: {e.detail}")
             return Response({"error": e.detail}, status=status.HTTP_404_NOT_FOUND)
-        # except Exception as e:
- #           return Response({"error": f"An unexpected error occour"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        except Exception as e:
+            logger.error(f"Internal Server Error: {e}")
+            return Response({"error": f"An unexpected error occour"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     return wrapper
